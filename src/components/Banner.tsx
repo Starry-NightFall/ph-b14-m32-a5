d6d9f5752685a2a8dfb7b9976d2810217909783f
Shahriar Hasan Siam <siamshahriarhasan@gmail.com>
2026-09-13 22:13:43 +0600

    created banner with image

---
+++ src/components/Banner.tsx
@@ -0,0 +1,23 @@
+import BannerImg from "../assets/banner-stack.png";
+
+const Banner = () => {
+  return (
+    <div className="hero bg-white">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img src={BannerImg} alt="Banner" className="max-w-sm rounded-lg" />
+        <div className="w-3/5 text-center lg:text-left text-black">
+          <h1 className="text-5xl font-bold">
+            Build Your Ideal
+            <br />
+            Development Stack
+          </h1>
+          <p className="py-6">Explore frontend, backend, databases, and tooling options. Compare them side by side and put together the stack that fits your next project. </p>
+          <button className="btn bg-linear-to-r from-orange-500 to-rose-500 mr-2">Explore Technologies</button>
+          <button className="btn">Learn More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
